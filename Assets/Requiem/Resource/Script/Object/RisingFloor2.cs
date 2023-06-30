b190ac223b1a95fe1af588d905c390fc9ca4c754
HideThat <101398304+HideThat@users.noreply.github.com>
2023-06-30 18:13:46 +0900

    2023-06-30

--- Assets/Requiem/Resource/Script/Object/RisingFloor2.cs
+++ Assets/Requiem/Resource/Script/Object/RisingFloor2.cs
@@ -3,14 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(RisingFloor2))]
+public class RisingFloor2Editor : Editor
+{
+    private SerializedProperty wallTriggerProp;
+    private SerializedProperty moveTimeProp;
+    private SerializedProperty moveDistanceProp;
+    private SerializedProperty isActiveProp;
+    private SerializedProperty showOtherProp;
+    private SerializedProperty targetPosProp;
+    private SerializedProperty originPosProp;
+    private SerializedProperty currentTimeProp;
+
+    private void OnEnable()
+    {
+        wallTriggerProp = serializedObject.FindProperty("wallTrigger");
+        moveTimeProp = serializedObject.FindProperty("moveTime");
+        moveDistanceProp = serializedObject.FindProperty("moveDistance");
+        isActiveProp = serializedObject.FindProperty("isActive");
+        showOtherProp = serializedObject.FindProperty("showOther");
+        targetPosProp = serializedObject.FindProperty("targetPos");
+        originPosProp = serializedObject.FindProperty("originPos");
+        currentTimeProp = serializedObject.FindProperty("currentTime");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(wallTriggerProp);
+        EditorGUILayout.PropertyField(moveTimeProp);
+        EditorGUILayout.PropertyField(moveDistanceProp);
+        EditorGUILayout.PropertyField(isActiveProp);
+        EditorGUILayout.PropertyField(showOtherProp);
+
+        RisingFloor2 risingFloor2 = (RisingFloor2)target;
+
+        if (risingFloor2.showOther)
+        {
+            EditorGUILayout.PropertyField(targetPosProp);
+            EditorGUILayout.PropertyField(originPosProp);
+            EditorGUILayout.PropertyField(currentTimeProp);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
+
+
 public class RisingFloor2 : MonoBehaviour
 {
     [SerializeField] public WallTrigger wallTrigger;
     [SerializeField] private float moveTime = 3f;
     [SerializeField] private float moveDistance = 3f;
     [SerializeField] public bool isActive = false;
-
     [Header("프로그래머")]
+    [SerializeField] public bool showOther = false;
     [SerializeField] Vector2 targetPos;
     [SerializeField] Vector2 originPos;
     [SerializeField] float currentTime = 5.9f;
